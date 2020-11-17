3ebbd5b56eafc04b386226146c707b714804562b
b166erobot <bernard0freitas@outlook.com>
2020-11-17 19:12:58 -0300

    sintaxe error resolvido

--- jogo/itens/vestes.py
+++ jogo/itens/vestes.py
@@ -1,7 +1,7 @@
 class Roupa:
     def __init__(
         self, vida = 0, dano = 0, resistencias = 0, velo_ataque = 0,
-        critico = 0, armadura = 0, velo_movi = 0
+        critico = 0, armadura = 0, velo_movi = 0, nome = ''
     ):
         self.vida = vida
         self.dano = dano
@@ -10,22 +10,23 @@ class Roupa:
         self.critico = critico
         self.armadura = armadura
         self.velo_movi = velo_movi
+        self.nome = nome
 
 
 class Peitoral(Roupa):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, nome = 'Peitoral', **kwargs)
 
 class Elmo(Roupa):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, nome = 'Elmo', **kwargs)
 
 class Calca(Roupa):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, nome = 'Calca', **kwargs)
 
 class Bota(Roupa):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, nome = 'Bota', **kwargs)
 
 tudo = [Peitoral, Elmo, Calca, Bota]
